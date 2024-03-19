8c4d362c1cec74871248363d48dcc22b1004463e
gcorral <gregorio.corral@gmail.com>
2024-03-19 15:51:36 -0400

    Fix spaces

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -133,8 +133,8 @@ class TestAccountService(TestCase):
         account = AccountFactory()
         response = self.client.post(
             BASE_URL,
-            json = account.serialize(),
-            content_type = "application/json"
+            json=account.serialize(),
+            content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -145,13 +145,11 @@ class TestAccountService(TestCase):
         updated_account = response.get_json()
         self.assertEqual(updated_account["name"], "John Doe")
 
-    
     def test_update_account_not_found(self):
         """It should not Update an Account not found"""
         account = AccountFactory()
         response = self.client.put(f"{BASE_URL}/0", json=account.serialize())
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    
 
     def test_get_account(self):
         """It should Read a single Account"""
@@ -163,12 +161,10 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)    
-
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
